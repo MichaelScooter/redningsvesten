4388ca1b4bef72eefb5738e02f6ff1e5468af69f
Michael <michaelscooterpoulsen@hotmail.com>
2023-12-03 21:01:42 +0100

    Start

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+
+    <title>Din Digitale Redningsvest</title>
+    <meta name="description" content="Digital Redningsvest - Gør det Nemt & Enkelt !">
+
+    <?php include "include/head.php"; ?>
+
+</head>
+<!------------------------------------------------- Body -------------------------------------------------------------->
+<body>
+<?php include "include/navigation.php"; ?>
+
+<div class="container-fluid bg-dark p-lg-5 vh-100">
+    <div class="row text-white pt-lg-5">
+        gfgfg
+    </div>
+
+
+</div>
+
+
+<!------------------------------------------------- Footer ------------------------------------------------------------>
+<?php include "include/footer.php"; ?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ reklame.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+
+    <title>Reklame</title>
+    <meta name="description" content="Digital Redningsvest - Slip for reklamer !">
+
+    <?php include "include/head.php"; ?>
+
+</head>
+<!------------------------------------------------- Body -------------------------------------------------------------->
+<body>
+<?php include "include/navigation.php"; ?>
+
+<div class="container-fluid bg-dark p-lg-5 vh-100">
+    <div class="row text-white pt-lg-5">
+        gfgfg
+    </div>
+
+
+</div>
+
+
+<!------------------------------------------------- Footer ------------------------------------------------------------>
+<?php include "include/footer.php"; ?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
